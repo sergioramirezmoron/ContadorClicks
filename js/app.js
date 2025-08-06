616999a6782273cfa6f8f4f5dd83eae74d37c2ac
sergioramirezmoron <sergiothd50@gmail.com>
2025-08-06 17:20:48 +0200

    estilos

--- js/app.js
+++ js/app.js
@@ -5,19 +5,26 @@ const input = document.getElementById("input");
 
 let contador = 0;
 
-bntAnadir.addEventListener("click", () => {
-  contador++;
-  input.textContent = contador;
-});
-
-bntEliminar.addEventListener("click", () => {
-  if (contador > 0) {
-    contador--;
+if (
+  bntAnadir != null ||
+  bntEliminar != null ||
+  bntReiniciar != null ||
+  input != null
+) {
+  bntAnadir.addEventListener("click", () => {
+    contador++;
     input.textContent = contador;
-  }
-});
+  });
 
-bntReiniciar.addEventListener("click", () => {
-  contador = 0;
-  input.textContent = contador;
-});
+  bntEliminar.addEventListener("click", () => {
+    if (contador > 0) {
+      contador--;
+      input.textContent = contador;
+    }
+  });
+
+  bntReiniciar.addEventListener("click", () => {
+    contador = 0;
+    input.textContent = contador;
+  });
+}
